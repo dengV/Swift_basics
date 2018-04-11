5f06b7175d73614da052cd39dc53dcb0b4d4ec31
邓江洲 <dengjingzhou@zto.cn>
2018-04-12 01:52:30 +0800

    collection view and I

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by dengjiangzhou on 2018/4/12.
+//  Copyright © 2018年 dengjiangzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Swift_basics
 Swift basics notes
+
+
+
+
+<hr>
+
+<hr>
+
+
+### git 操作
+
+<ul>
+<li>
+git push git@dengV:dengV/Swift_basics.git
+</li>
+
+</ul>
