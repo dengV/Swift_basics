509c04b5f2491f050b331dc997868a564eccf19c
邓江洲 <dengjingzhou@zto.cn>
2018-04-19 00:35:21 +0800

    collection view start chap 3 OK

--- CollectionView/CollectionView/ViewController.swift
+++ CollectionView/CollectionView/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    
+    
+    var collectionData = ["1 🏆", "2 🐸", "3 🍩", "4 😸", "5 🤡", "6 👾", "7 👻", "8 👩‍🎤", "9 🎸", "10 🍖", "11 🐯", "12 🌋"]
+    
+    
+    /* var collectionData = ["1 🍏", "2 🍒", "3 🍈", "4 🥑",
+                          "5 🥝", "6 🧀", "7 🥚", "8 🥩",
+                          "9 🍔", "10 🥙", "11 🥘", "12 ⛳️"]
+    */
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = (view.frame.size.width - 20) / 3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,16 +47,19 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return collectionData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCellIdentifier", for: indexPath)
+        
+        if let label = cell.viewWithTag(100) as? UILabel{
+            label.text = collectionData[indexPath.row]
+        }
+        return cell
     }
-    
-    
-    
-    
+
     
 }
 
